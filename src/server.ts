7272c4642f5ecf54a2d3646912e26b8c4e58da23
unknown <somgeanesombra0148@digitalcollege.internal>
2026-06-23 22:29:12 -0300

    feat: implementar configuracao do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,44 @@
-import http from 'node:http';
+//import http from 'node:http';
 
+// const users = [
+// 	{
+// 		name: 'Cristiano Ronaldo',
+// 		status: true,
+// 	},
+// 	{
+// 		name: ' Lionel Messi',
+// 		status: true,
+// 	},
+
+// 	{
+// 		name: 'Neymar Junior',
+// 		status: false,
+// 	},
+// ];
+
+// http
+// 	.createServer((request, response) => {
+// 		if (request.url === '/users' && request.method === 'GET') {
+// 			response.writeHead(200, {
+// 				'content-type': 'application/json',
+// 			});
+
+// 			response.end(JSON.stringify(users));
+// 			return;
+// 		} else {
+// 			response.writeHead(404, {
+// 				'content-type': 'text/plain',
+// 			});
+// 			response.end('not found!');
+// 		}
+// 	})
+// 	.listen(Number(process.env.PORT));
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
 const users = [
 	{
 		name: 'Cristiano Ronaldo',
@@ -16,20 +55,8 @@ const users = [
 	},
 ];
 
-http
-	.createServer((request, response) => {
-		if (request.url === '/users' && request.method === 'GET') {
-			response.writeHead(200, {
-				'content-type': 'application/json',
-			});
+app.get('/users', (request, response) => {
+	response.json(users);
+});
 
-			response.end(JSON.stringify(users));
-			return;
-		} else {
-			response.writeHead(404, {
-				'content-type': 'text/plain',
-			});
-			response.end('not found!');
-		}
-	})
-	.listen(Number(process.env.PORT));
+app.listen(Number(process.env.PORT));
