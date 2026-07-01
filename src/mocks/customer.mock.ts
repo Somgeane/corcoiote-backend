77ddb83c4d9019dd06e00916649a2e7fa74a4d93
unknown <somgeanesombra0148@digitalcollege.internal>
2026-06-30 22:14:16 -0300

    refactor: mover dados simulados para diretorio dedicado

---
+++ src/mocks/customer.mock.ts
@@ -0,0 +1,23 @@
+import type { Customer } from '../types.ts';
+
+export const customers: Customer[] = [
+	{
+		id: 1,
+		name: 'Cristiano Ronaldo',
+		email: 'cris@ElementInternals.com',
+		status: true,
+	},
+	{
+		id: 2,
+		name: ' Lionel Messi',
+		email: 'messi@email.com',
+		status: true,
+	},
+
+	{
+		id: 3,
+		name: 'Neymar Junior',
+		email: 'ney@email.com',
+		status: false,
+	},
+];
