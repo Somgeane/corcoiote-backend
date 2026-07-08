7852a33dfefc59b0cdd0447fa573acd27e27f5cb
unknown <somgeanesombra0148@digitalcollege.internal>
2026-07-07 21:46:48 -0300

    feat:adicionar ponto de entrada para recurso de clientes

--- src/server.ts
+++ src/server.ts
@@ -1,62 +1,16 @@
-//import http from 'node:http';
-
-// const users = [
-// 	{
-// 		name: 'Cristiano Ronaldo',
-// 		status: true,
-// 	},
-// 	{
-// 		name: ' Lionel Messi',
-// 		status: true,
-// 	},
-
-// 	{
-// 		name: 'Neymar Junior',
-// 		status: false,
-// 	},
-// ];
-
-// http
-// 	.createServer((request, response) => {
-// 		if (request.url === '/users' && request.method === 'GET') {
-// 			response.writeHead(200, {
-// 				'content-type': 'application/json',
-// 			});
-
-// 			response.end(JSON.stringify(users));
-// 			return;
-// 		} else {
-// 			response.writeHead(404, {
-// 				'content-type': 'text/plain',
-// 			});
-// 			response.end('not found!');
-// 		}
-// 	})
-// 	.listen(Number(process.env.PORT));
-
 import express from 'express';
+import CustomersRouter from './routes/customers.routes.ts';
 
 const app = express();
 
 app.use(express.json());
-const users = [
-	{
-		name: 'Cristiano Ronaldo',
-		status: true,
-	},
-	{
-		name: ' Lionel Messi',
-		status: true,
-	},
 
-	{
-		name: 'Neymar Junior',
-		status: false,
-	},
-];
+app.use('/customers', CustomersRouter);
 
-app.get('/users', (_request, response) => {
-	response.json(users);
+app.use((_request, response) => {
+	response.status(404).json({
+		message: 'Not found!',
+	});
 });
 
 app.listen(Number(process.env.PORT));
