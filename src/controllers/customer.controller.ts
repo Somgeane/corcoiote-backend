ce54dc8fdfd86d5969d0c482dc9a303b59a145e0
unknown <somgeanesombra0148@digitalcollege.internal>
2026-07-07 21:26:48 -0300

    feat: estabelecer camada de controlador para recurso de clientes

---
+++ src/controllers/customer.controller.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from 'express';
+import { customers } from '../mocks/customer.mock.ts';
+import * as CustomerService from '../services/customer.service.ts';
+import type { CreateCustumer, UpdateCustomer } from '../types.ts';
+
+export function getAllCustomers(request: Request, response: Response) {
+	const customers = CustomerService.findAllCustomers();
+	response.status(200).json(customers);
+}
+
+export function getCustomersById(request: Request, response: Response) {
+	const id = Number(request.params.id);
+
+	const customer = CustomerService.findCustomerById(id);
+
+	response.status(200).json(customer);
+}
+
+export function createCustomers(request: Request, response: Response) {
+	const { name, email } = request.body as CreateCustumer;
+	const customer = CustomerService.insertCustomer({ name, email });
+	response.status(201).json(customer);
+}
+
+export function updateCustomers(request: Request, response: Response) {
+	const id = Number(request.params.id);
+	const { name, email, status } = request.body as UpdateCustomer;
+	const customer = CustomerService.modifyCustomer(id, {
+		name,
+		email,
+		status,
+	});
+	response.status(200).json(customer);
+}
+
+export function deleteCustomers(request: Request, response: Response) {
+	const id = Number(request.params.id);
+	CustomerService.removeCustomer(id);
+	response.status(204).send();
+}
